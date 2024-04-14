45b22aa7f4ea869910ed2ee2dd1fea7b30896707
PratikThapa24 <prajwaljungthapa@gmail.com>
2024-04-14 11:15:54 -0400

    Added feature of sending email

--- boot_personWebsit/main.py
+++ boot_personWebsit/main.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from post import Post
+import smtplib
+import os
 
 app = Flask(__name__)
+
 
 url_blog_post = f"https://api.npoint.io/c790b4d5cab58020d391"
 response = requests.get(url_blog_post)
 json_response = response.json()
+email = "prajwaljungthapa@gmail.com"
+password = os.environ.get("PASS")
+
 
 post_object = []
 for post in json_response:
@@ -18,9 +24,26 @@ for post in json_response:
 def home():
     return render_template("index.html", json = post_object)
 
-@app.route('/contact')
+
+@app.route('/contact', methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent = True)
+    return render_template("contact.html", msg_sent = False)
+    
+def send_email(name , email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    try:
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(email, password)
+            connection.sendmail(email, email, email_message)
+        print("Email sent successfully.")
+    except Exception as e:
+        print(f"Error sending email: {e}")
+
 
 @app.route('/about')
 def about():
